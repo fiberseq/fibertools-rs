5814c445ef4ec8dfeb97716e0cde4815130f5918
Mitchell Robert Vollger <mrvollger@gmail.com>
2022-07-30 11:54:22 -0700

    try again

--- src/center.rs
+++ src/center.rs
@@ -74,6 +74,7 @@ impl CenteredFiberData {
     }
 
     pub fn cpg_positions(&self) -> Vec<i64> {
+        // TODO adjust cpg on minus strand by -1
         self.apply_offset(&self.fiber.base_mods.cpg_positions(false))
     }
 
