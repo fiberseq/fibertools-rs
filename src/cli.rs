ae48073f487515c473354299ba385aa825f5c283
Mitchell Robert Vollger <mrvollger@gmail.com>
2022-11-21 13:35:48 -0800

    Stadardize message formating

--- src/cli.rs
+++ src/cli.rs
@@ -14,7 +14,7 @@ use clap::{AppSettings, Parser, Subcommand};
 )]
 #[clap(global_setting(AppSettings::DeriveDisplayOrder))]
 pub struct Cli {
-    /// Threads for decompression.
+    /// Threads for decompression
     #[clap(
         global = true,
         short,
@@ -23,7 +23,7 @@ pub struct Cli {
         help_heading = "GLOBAL-OPTIONS"
     )]
     pub threads: usize,
-    /// Logging level [-v: Info, -vv: Debug, -vvv: Trace].
+    /// Logging level [-v: Info, -vv: Debug, -vvv: Trace]
     #[clap(
         global = true,
         short,
@@ -32,7 +32,7 @@ pub struct Cli {
         help_heading = "DEBUG-OPTIONS"
     )]
     pub verbose: usize,
-    /// Turn of all logging.
+    /// Turn of all logging
     #[clap(global = true, long, help_heading = "DEBUG-OPTIONS")]
     pub quiet: bool,
     /// Subcommands for fibertools-rs
@@ -45,72 +45,72 @@ pub struct Cli {
 ///
 #[derive(Subcommand, Debug)]
 pub enum Commands {
-    /// Extract fiberseq data into plain text files.
+    /// Extract fiberseq data into plain text files
     #[clap(visible_aliases = &["ex", "e"])]
     Extract {
-        /// fiberseq bam file
+        /// Fiberseq bam file
         #[clap(default_value = "-")]
         bam: String,
-        /// report in reference sequence coordinates.
+        /// Report in reference sequence coordinates
         #[clap(short, long)]
         reference: bool,
-        /// Minium score in the ML tag to include in the output.
+        /// Minium score in the ML tag to include in the output
         #[clap(short, long, default_value = "150")]
         min_ml_score: u8,
-        /// Output path for m6a bed12.
+        /// Output path for m6a bed12
         #[clap(long)]
         m6a: Option<String>,
-        /// Output path for 5mC (CpG, primrose) bed12.
+        /// Output path for 5mC (CpG, primrose) bed12
         #[clap(short, long)]
         cpg: Option<String>,
-        /// Output path for methylation sensitive patch (msp) bed12.
+        /// Output path for methylation sensitive patch (msp) bed12
         #[clap(long)]
         msp: Option<String>,
-        /// Output path for nucleosome bed12.
+        /// Output path for nucleosome bed12
         #[clap(short, long)]
         nuc: Option<String>,
-        /// Output path for a tabular format including "all" fiberseq information in the bam.
+        /// Output path for a tabular format including "all" fiberseq information in the bam
         #[clap(short, long)]
         all: Option<String>,
-        /// include per base quality scores in "fiber_qual"
+        /// Include per base quality scores in "fiber_qual"
         #[clap(short, long, help_heading = "ALL-FORMATTING-OPTIONS")]
         quality: bool,
         /// Add the full floating point predictions of the ML model
         #[clap(short, long, help_heading = "ALL-FORMATTING-OPTIONS")]
         full_float: bool,
-        /// simplify output by remove fiber sequence
+        /// Simplify output by remove fiber sequence
         #[clap(short, long, help_heading = "ALL-FORMATTING-OPTIONS")]
         simplify: bool,
     },
-    /// This command centers fiberseq data around given reference positions. This is useful for making aggregate m6a and CpG observations, as well as visualization of SVs.
+    /// This command centers fiberseq data around given reference positions. This is useful for making aggregate m6a and CpG observations, as well as visualization of SVs
     #[clap(visible_aliases = &["c", "ct"])]
     Center {
-        /// fiberseq bam file, must be aligned and have an index.
+        /// Fiberseq bam file, must be aligned and have an index
         bam: String,
-        /// Bed file on which to center fiberseq reads. Data is adjusted to the start position of the bed file and corrected for strand if a 4th strand column is included.
+        /// Bed file on which to center fiberseq reads. Data is adjusted to the start position of the bed file and corrected for strand if a 4th strand column is included
         ///
         /// If you include strand information in the 4th column it will orient data accordingly and use the end position of bed record instead of the start if on the minus strand. This means that profiles of motifs in both the forward and minus orientation will align to the same central position.
         bed: String,
-        /// Minium score in the ML tag to include in the output.
+        /// Minium score in the ML tag to include in the output
         #[clap(short, long, default_value = "150")]
         min_ml_score: u8,
-        /// Provide data in wide format, one row per read.
+        /// Provide data in wide format, one row per read
         #[clap(short, long)]
         wide: bool,
     },
-    /// Predict m6A positions using HiFi kinetics data and encode the results in the MM and ML bam tags.
+    /// Predict m6A positions using HiFi kinetics data and encode the results in the MM and ML bam tags
     #[clap(visible_aliases = &["m6A", "m6a"])]
     PredictM6A {
-        /// Bam HiFi file with kinetics.
+        /// Bam HiFi file with kinetics
         #[clap(default_value = "-")]
         bam: String,
-        /// Output bam file with m6A calls in new/extended MM and ML bam tags.
+        /// Output bam file with m6A calls in new/extended MM and ML bam tags
         #[clap(default_value = "-")]
         out: String,
-        /// keep hifi kinetics data
+        /// Keep hifi kinetics data
         #[clap(short, long)]
         keep: bool,
-        /// use CNN model for prediction instead of XGB.
+        /// Use CNN model for prediction instead of XGBoost
         #[clap(short, long)]
         cnn: bool,
         /// Add a bam tag (mp) with the full floating point predictions of the ML model
