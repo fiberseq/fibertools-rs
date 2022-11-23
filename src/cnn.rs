a928875bb494e27a65a1b6d178f70923fc9e446f
Mitchell Robert Vollger <mrvollger@gmail.com>
2022-11-23 09:22:17 -0800

    move model to gpu?

--- src/cnn.rs
+++ src/cnn.rs
@@ -24,9 +24,10 @@ pub fn get_saved_pytorch_model(polymerase: &PbChem) -> &'static tch::CModule {
         };
         let temp_file_name = "ft.tmp.model.json";
         fs::write(temp_file_name, model_str).expect("Unable to write file");
-        let model = tch::CModule::load(temp_file_name).expect("Unable to load PyTorch model");
+        let mut temp_path = fs::File::open(temp_file_name).expect("Unable to open model file.");
+        let model = tch::CModule::load_data_on_device(&mut temp_path, d)
+            .expect("Unable to load PyTorch model");
         fs::remove_file(temp_file_name).expect("Unable to remove temp model file");
-        log::info!("Model from PyTorch loaded");
         //tch::nn::VarStore::set_device(&mut self, device)
         model
     })
@@ -36,7 +37,6 @@ pub fn predict_with_cnn(windows: &[f32], count: usize, polyermase: &PbChem) -> V
     let model = get_saved_pytorch_model(polyermase);
     let ts = tch::Tensor::of_slice(windows).to_device(tch::Device::cuda_if_available());
     let ts = ts.reshape(&[count.try_into().unwrap(), 6, 15]);
-    log::warn!("Made other gpu? tensor");
     let x = model.forward_ts(&[ts]).unwrap();
     let w: Vec<f32> = x.try_into().unwrap();
     let z: Vec<f32> = w.chunks(2).map(|c| c[0]).collect();
