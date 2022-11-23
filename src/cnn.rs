a213c7c03d5163a701246ce5dbc630db4337ebf9
Mitchell Robert Vollger <mrvollger@gmail.com>
2022-11-23 08:54:34 -0800

    move vec to gpu?

--- src/cnn.rs
+++ src/cnn.rs
@@ -10,10 +10,8 @@ static PT_2_2: &[u8] = include_bytes!("../models/2.0_torch.pt");
 
 pub fn get_saved_pytorch_model(polymerase: &PbChem) -> &'static tch::CModule {
     INIT_PT.call_once(|| {
-        log::warn!(
-            "Using {:?} for Torch device.",
-            tch::Device::cuda_if_available()
-        );
+        let d = tch::Device::cuda_if_available();
+        log::warn!("Using {:?} for Torch device.", d);
         let model_str = match polymerase {
             PbChem::Two => {
                 log::warn!("Using model for 2.0 chemistry");
@@ -29,13 +27,14 @@ pub fn get_saved_pytorch_model(polymerase: &PbChem) -> &'static tch::CModule {
         let model = tch::CModule::load(temp_file_name).expect("Unable to load PyTorch model");
         fs::remove_file(temp_file_name).expect("Unable to remove temp model file");
         log::info!("Model from PyTorch loaded");
+        //tch::nn::VarStore::set_device(&mut self, device)
         model
     })
 }
 
 pub fn predict_with_cnn(windows: &[f32], count: usize, polyermase: &PbChem) -> Vec<f32> {
     let model = get_saved_pytorch_model(polyermase);
-    let ts = tch::Tensor::of_slice(windows);
+    let ts = tch::Tensor::of_slice(windows).to_device(tch::Device::cuda_if_available());
     let ts = ts.reshape(&[count.try_into().unwrap(), 6, 15]);
     let x = model.forward_ts(&[ts]).unwrap();
     let w: Vec<f32> = x.try_into().unwrap();
