b45a29f79922690a66063a70ff562e1c1fe0027d
Mitchell Robert Vollger <mrvollger@gmail.com>
2022-12-06 11:09:04 -0800

    Add version to git hash

--- build.rs
+++ build.rs
@@ -7,5 +7,9 @@ fn main() {
         .output()
         .unwrap();
     let git_hash = String::from_utf8(output.stdout).unwrap();
-    println!("cargo:rustc-env=GIT_HASH={}", git_hash);
+    let version = env!("CARGO_PKG_VERSION");
+    println!(
+        "cargo:rustc-env=GIT_HASH=version:{} commit:{}'",
+        version, git_hash
+    );
 }
