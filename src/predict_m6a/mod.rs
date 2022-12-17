924c825581c0c7ead36d5473d423c25752a3b961
Mitchell Robert Vollger <mrvollger@gmail.com>
2022-12-16 16:14:23 -0800

    Update model

--- src/predict_m6a/mod.rs
+++ src/predict_m6a/mod.rs
@@ -57,15 +57,8 @@ impl PredictOptions {
 
     pub fn float_to_u8(&self, x: f32) -> u8 {
         if self.semi {
-            if (x > 0.0) & (x <= 0.05) {
-                let t = (-1000.0 * (1.0 - x).log10()).round() as u8;
-                if t > 13 {
-                    13
-                } else {
-                    t
-                }
-            } else if x > 0.05 {
-                (x * 255.0).round() as u8
+            if x > 0.0 {
+                (255.0 * -10.0 * (1.0 - x).log10()).round() as u8
             } else {
                 0
             }
