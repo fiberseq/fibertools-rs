b127d8033e0d4a489a98009745d88bb791e31674
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-04-22 16:38:20 -0700

    updating to new revio models

--- src/predict_m6a/cnn.rs
+++ src/predict_m6a/cnn.rs
@@ -12,13 +12,13 @@ pub static PT_2_2: &[u8] = include_bytes!("../../models/2.2_torch.pt");
 pub static SEMI: &[u8] = include_bytes!("../../models/2.0_semi_torch.pt");
 pub static SEMI_2_2: &[u8] = include_bytes!("../../models/2.2_semi_torch.pt");
 pub static SEMI_3_2: &[u8] = include_bytes!("../../models/3.2_semi_torch.pt");
-pub static SEMI_REVIO: &[u8] = include_bytes!("../../models/Revio_semi_torch.2.pt");
+pub static SEMI_REVIO: &[u8] = include_bytes!("../../models/Revio_semi_torch.pt");
 
 // json precision tables
 pub static SEMI_JSON_2_0: &str = include_str!("../../models/2.0_semi_torch.json");
 pub static SEMI_JSON_2_2: &str = include_str!("../../models/2.2_semi_torch.json");
 pub static SEMI_JSON_3_2: &str = include_str!("../../models/3.2_semi_torch.json");
-pub static SEMI_JSON_REVIO: &str = include_str!("../../models/Revio_semi_torch.2.json");
+pub static SEMI_JSON_REVIO: &str = include_str!("../../models/Revio_semi_torch.json");
 
 pub fn get_saved_pytorch_model(predict_options: &PredictOptions) -> &'static tch::CModule {
     INIT_PT.call_once(|| {
