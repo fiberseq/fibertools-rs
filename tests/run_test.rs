6fa98e710724b3ce783234020839619b5406171f
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-06-14 18:05:56 -0700

    Add run tests

---
+++ tests/run_test.rs
@@ -0,0 +1,69 @@
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use std::process::Command; // Run programs
+
+#[test]
+fn test_m6a() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("ft")?;
+    cmd.arg("m6a")
+        .arg("-v")
+        .arg(".test/all.bam")
+        .arg("/dev/null");
+    cmd.assert()
+        .success()
+        .stderr(predicate::str::contains("done! Time elapsed:"));
+    Ok(())
+}
+
+#[test]
+fn test_center() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("ft")?;
+    cmd.arg("center")
+        .arg("-v")
+        .arg(".test/center.bam")
+        .arg(".test/center.bed");
+    cmd.assert()
+        .success()
+        .stderr(predicate::str::contains("done! Time elapsed:"));
+    Ok(())
+}
+
+#[test]
+fn test_extract() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("ft")?;
+    cmd.arg("extract")
+        .arg("-v")
+        .arg(".test/all.bam")
+        .arg("--all")
+        .arg("/dev/null");
+    cmd.assert()
+        .success()
+        .stderr(predicate::str::contains("done! Time elapsed:"));
+    Ok(())
+}
+
+#[test]
+fn test_nucleosomes() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("ft")?;
+    cmd.arg("add-nucleosomes")
+        .arg("-v")
+        .arg(".test/all.bam")
+        .arg("/dev/null");
+    cmd.assert()
+        .success()
+        .stderr(predicate::str::contains("done! Time elapsed:"));
+    Ok(())
+}
+
+#[test]
+fn test_clear() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("ft")?;
+    cmd.arg("clear")
+        .arg("-v")
+        .arg(".test/all.bam")
+        .arg("/dev/null");
+    cmd.assert()
+        .success()
+        .stderr(predicate::str::contains("done! Time elapsed:"));
+    Ok(())
+}
