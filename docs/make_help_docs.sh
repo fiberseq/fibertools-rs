b93908f3335913d880b2b934239ab33e439889a1
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-07-18 12:10:38 -0700

    reorganize test data

--- docs/make_help_docs.sh
+++ docs/make_help_docs.sh
@@ -14,5 +14,5 @@ done
 
 # extract the column names for the all command
 printf '# Columns in `ft extract --all`\n```\n' >docs/ft-all-columns.md
-cargo run -- -v extract --all - .test/all.bam | head -n 1 | sed 's/\t/\n/g' >>docs/ft-all-columns.md
+cargo run -- -v extract --all - tests/data/all.bam | head -n 1 | sed 's/\t/\n/g' >>docs/ft-all-columns.md
 echo '```' >>docs/ft-all-columns.md
