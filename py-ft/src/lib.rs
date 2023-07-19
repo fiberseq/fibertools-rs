7e5fbde9f0f52496fd1f2facbbd55d80d0b64a26
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-07-19 14:53:11 -0700

    start a python package

---
+++ py-ft/src/lib.rs
@@ -0,0 +1,23 @@
+mod fiberdata;
+
+use pyo3::prelude::*;
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// Formats the sum of two numbers as string.
+#[pyfunction]
+fn read_fibertools_bam(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn py_ft(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(read_fibertools_bam, m)?)?;
+    Ok(())
+}
