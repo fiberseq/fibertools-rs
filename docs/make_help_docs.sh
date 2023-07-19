8820db20f217b04ba0f9fd25a87af81818488432
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-07-18 19:50:31 -0700

    update docs

--- docs/make_help_docs.sh
+++ docs/make_help_docs.sh
@@ -4,7 +4,7 @@ echo $LIBTORCH
 export LD_LIBRARY_PATH=${LIBTORCH}/lib:$LD_LIBRARY_PATH
 export DYLD_LIBRARY_PATH=${LIBTORCH}/lib:$LD_LIBRARY_PATH
 
-for subcommand in "" "extract" "center" "predict-m6a" "add-nucleosomes" "clear-kinetics"; do
+for subcommand in "" "extract" "center" "predict-m6a" "add-nucleosomes" "clear-kinetics" "strip-basemods"; do
     echo $subcommand
     out="docs/ft-${subcommand}-help.md"
     echo '```' >$out
