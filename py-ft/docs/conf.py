d4b75b1a569d694495bc1e87de651c1bb4e63778
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-07-19 21:04:54 -0700

    chore: python docs

---
+++ py-ft/docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../"))
+
+import py_ft
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+# The short X.Y version.
+project = "py-ft"
+copyright = "2023, Mitchell R. Vollger"
+author = "Mitchell R. Vollger"
+version = py_ft.__version__
+# The full version, including alpha/beta/rc tags.
+release = py_ft.__version__
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.viewcode", "sphinx.ext.napoleon"]
+
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+# html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
