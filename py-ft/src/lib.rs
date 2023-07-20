b03df77d99a8f28ecd6d43509a43ff92fb300329
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-07-20 12:38:24 -0700

    chore: rename iterator

--- py-ft/src/lib.rs
+++ py-ft/src/lib.rs
@@ -7,7 +7,7 @@ use pyo3::prelude::*;
 #[pymodule]
 fn pyft(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
-    m.add_class::<fiberdata::FiberdataIter>()?;
+    m.add_class::<fiberdata::FiberdataFetch>()?;
     m.add_class::<fiberdata::Fiberdata>()?;
     m.add_class::<fiberdata::Basemods>()?;
     m.add_class::<fiberdata::Ranges>()?;
