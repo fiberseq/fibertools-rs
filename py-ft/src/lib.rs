cd816481ddc858ba9459a1e33e22617ad07a532e
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-07-20 10:05:56 -0700

    chore: docs

--- py-ft/src/lib.rs
+++ py-ft/src/lib.rs
@@ -5,7 +5,7 @@ use pyo3::prelude::*;
 
 /// A python module for using rust to access data from fiberseq BAM files.
 #[pymodule]
-fn py_ft(_py: Python, m: &PyModule) -> PyResult<()> {
+fn pyft(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     m.add_class::<fiberdata::FiberdataIter>()?;
     m.add_class::<fiberdata::PyFiberdata>()?;
