32f01826bbf06ac726464d59eee2769dfe80b138
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-07-19 22:11:19 -0700

    chore: python docs

--- py-ft/src/lib.rs
+++ py-ft/src/lib.rs
@@ -8,5 +8,6 @@ use pyo3::prelude::*;
 fn py_ft(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add("__version__", env!("CARGO_PKG_VERSION"))?;
     m.add_class::<fiberdata::FiberdataIter>()?;
+    m.add_class::<fiberdata::PyFiberdata>()?;
     Ok(())
 }
