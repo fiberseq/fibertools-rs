248bf4e56c5b10c2dd4613888818a995e6f75529
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-07-22 13:51:53 -0700

    feat: adding liftover to pyft

--- py-ft/example.py
+++ py-ft/example.py
@@ -7,4 +7,6 @@ fiberdata = pyft.FiberdataFetch(bam_f, "chr20", 0, 10_000_000)
 for idx, fiber in enumerate(tqdm.tqdm(fiberdata)):
     if idx < 10:
         print(fiber)
-    fiber.get_aligned_ranges()
+    fiber.get_aligned_blocks_as_ranges()
+    fiber.lift_query_positions([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    fiber.lift_reference_positions([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
