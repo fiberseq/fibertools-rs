8bf2168969acf67fa5069fb578f94941bd01e3f4
Mitchell Robert Vollger <mrvollger@gmail.com>
2023-07-22 14:50:11 -0700

    chore: readme

--- py-ft/example.py
+++ py-ft/example.py
@@ -1,11 +1,12 @@
 import pyft
 import tqdm
+import sys
 
 bam_f = "../tmp.bam"
 fiberdata = pyft.FiberdataFetch(bam_f, "chr20", 0, 10_000_000)
 for idx, fiber in enumerate(tqdm.tqdm(fiberdata)):
     if idx < 10:
-        print(fiber)
+        print(fiber, file=sys.stderr)
     
     fiber.get_aligned_blocks_as_ranges()
     fiber.lift_query_positions([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
