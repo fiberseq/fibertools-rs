250f080b65db4e4fd78f9edad6acf425a3ecdd78
Mitchell R. Vollger <mrvollger@gmail.com>
2023-07-30 13:31:30 -0700

    fmt

--- py-ft/example.py
+++ py-ft/example.py
@@ -1,9 +1,9 @@
 import pyft
 import tqdm
 
-bam_f = "../tmp.bam"
+bam_f = "../tests/data/center.bam"
 fiberbam = pyft.Fiberbam(bam_f)
-for fiber in tqdm.tqdm(fiberbam.fetch("chr20", 10_000_000, 30_000_000)): 
+for fiber in tqdm.tqdm(fiberbam.fetch("chr1", 0, 260_000_000)):
     # the number of ccs passes
     fiber.ec
     # the mps start positions
@@ -14,7 +14,7 @@ for fiber in tqdm.tqdm(fiberbam.fetch("chr20", 10_000_000, 30_000_000)):
     fiber.lift_query_positions([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
     # lift reference positions to query (fiber) positions
     fiber.lift_reference_positions([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
- 
+
 
 for fiber in fiberbam.center("chr20", start=20_000_000, end=20_000_001, strand="-"):
     # returns the same fiber object as above; however, all the positions have been modified to be relative to the region fetched
