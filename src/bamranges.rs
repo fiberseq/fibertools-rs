a24ce8ee1b1b169371cfcf0fe9b378266045994d
Mitchell Robert Vollger <mrvollger@gmail.com>
2024-01-12 20:18:44 -0800

    Refactor (#38)
    refactor fiber data items to share the `Ranges` datastructure

---
+++ src/bamranges.rs
@@ -0,0 +1,298 @@
+use super::bamlift::*;
+use itertools::{izip, multiunzip};
+use rust_htslib::bam;
+
+#[derive(Debug, Clone, PartialEq, Eq, Ord, PartialOrd)]
+pub struct Ranges {
+    pub starts: Vec<Option<i64>>,
+    pub ends: Vec<Option<i64>>,
+    pub lengths: Vec<Option<i64>>,
+    pub qual: Vec<u8>,
+    pub reference_starts: Vec<Option<i64>>,
+    pub reference_ends: Vec<Option<i64>>,
+    pub reference_lengths: Vec<Option<i64>>,
+    pub seq_len: i64,
+    reverse: bool,
+}
+
+impl Ranges {
+    /// starts and ends are [) intervals.
+    pub fn new(
+        record: &bam::Record,
+        mut forward_starts: Vec<i64>,
+        forward_ends: Option<Vec<i64>>,
+        mut lengths: Option<Vec<i64>>,
+    ) -> Self {
+        let mut single_bp_liftover = false;
+        // assume ends == starts if not provided
+        if forward_ends.is_none() && lengths.is_none() {
+            lengths = Some(vec![1; forward_starts.len()]);
+            single_bp_liftover = true;
+        }
+
+        // use ends or calculate them
+        let mut forward_ends_inclusive: Vec<i64> = match forward_ends {
+            Some(x) => x.into_iter().map(|x| x + 1).collect(),
+            None => forward_starts
+                .iter()
+                .zip(lengths.unwrap().iter())
+                .map(|(&x, &y)| x + y - 1)
+                .collect(),
+        };
+
+        // bam features for finding aligned positions
+        let is_reverse = record.is_reverse();
+        let seq_len = i64::try_from(record.seq_len()).unwrap();
+
+        // get positions and lengths in reference orientation
+        Self::positions_on_aligned_sequence(&mut forward_starts, is_reverse, seq_len);
+        Self::positions_on_aligned_sequence(&mut forward_ends_inclusive, is_reverse, seq_len);
+        let mut starts = forward_starts;
+        let mut ends = forward_ends_inclusive;
+
+        // swaps starts and ends if we reverse complemented
+        if record.is_reverse() {
+            std::mem::swap(&mut starts, &mut ends);
+        }
+
+        // swap back to non-inclusive ends
+        ends = ends.into_iter().map(|x| x + 1).collect();
+
+        // get lengths
+        let lengths = starts
+            .iter()
+            .zip(ends.iter())
+            .map(|(&x, &y)| Some(y - x))
+            .collect::<Vec<_>>();
+
+        let (reference_starts, reference_ends, reference_lengths) = if single_bp_liftover {
+            lift_query_range_exact(record, &starts, &starts)
+        } else {
+            lift_query_range(record, &starts, &ends)
+        };
+
+        // return object
+        Ranges {
+            starts: starts.into_iter().map(Some).collect(),
+            ends: ends.into_iter().map(Some).collect(),
+            lengths,
+            qual: vec![0; reference_starts.len()],
+            reference_starts,
+            reference_ends,
+            reference_lengths,
+            seq_len,
+            reverse: is_reverse,
+        }
+    }
+
+    pub fn set_qual(&mut self, qual: Vec<u8>) {
+        assert_eq!(qual.len(), self.starts.len());
+        self.qual = qual;
+        if self.reverse {
+            self.qual.reverse();
+        }
+    }
+
+    /// get positions on the complimented sequence in the cigar record
+    fn positions_on_aligned_sequence(input_positions: &mut [i64], is_reverse: bool, seq_len: i64) {
+        if !is_reverse {
+            return;
+        }
+        //need to correct for going from [) to (] if we are part of a range
+        for p in input_positions.iter_mut() {
+            *p = seq_len - *p - 1;
+        }
+        input_positions.reverse();
+    }
+
+    /// get the molecular coordinates of the ranges, taking into account
+    /// the alignment orientation
+    pub fn get_molecular(&self) -> Vec<Option<(i64, i64, i64)>> {
+        self.starts
+            .iter()
+            .zip(self.ends.iter())
+            .zip(self.lengths.iter())
+            .map(|((s, e), l)| {
+                if let (Some(s), Some(e), Some(l)) = (s, e, l) {
+                    Some((*s, *e, *l))
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
+
+    pub fn get_starts(&self) -> Vec<i64> {
+        self.starts.clone().into_iter().flatten().collect()
+    }
+
+    pub fn get_ends(&self) -> Vec<i64> {
+        self.ends.clone().into_iter().flatten().collect()
+    }
+
+    pub fn get_forward_starts(&self) -> Vec<i64> {
+        let mut z = self.get_starts();
+        Self::positions_on_aligned_sequence(&mut z, self.reverse, self.seq_len);
+        z
+    }
+
+    pub fn get_forward_quals(&self) -> Vec<u8> {
+        let mut forward = self.qual.clone();
+        if self.reverse {
+            forward.reverse();
+        }
+        forward
+    }
+
+    pub fn to_strings(&self, reference: bool, skip_none: bool) -> Vec<String> {
+        let (s, e, l, q) = if reference {
+            (
+                &self.reference_starts,
+                &self.reference_ends,
+                &self.reference_lengths,
+                &self.qual,
+            )
+        } else {
+            (&self.starts, &self.ends, &self.lengths, &self.qual)
+        };
+
+        let s = super::join_by_str_option_can_skip(s, ",", skip_none);
+        let e = super::join_by_str_option_can_skip(e, ",", skip_none);
+        let l = super::join_by_str_option_can_skip(l, ",", skip_none);
+        if reference {
+            vec![s, e, l]
+        } else {
+            let q = super::join_by_str(q, ",");
+            vec![s, e, l, q]
+        }
+    }
+
+    /// get the reference coordinates of the ranges, taking into account
+    /// the alignment orientation
+    pub fn get_reference(&self) -> Vec<Option<(i64, i64, i64)>> {
+        self.reference_starts
+            .iter()
+            .zip(self.reference_ends.iter())
+            .zip(self.reference_lengths.iter())
+            .map(|((s, e), l)| {
+                if let (Some(s), Some(e), Some(l)) = (s, e, l) {
+                    Some((*s, *e, *l))
+                } else {
+                    None
+                }
+            })
+            .collect()
+    }
+
+    pub fn merge_ranges(multiple_ranges: Vec<&Self>) -> Self {
+        if multiple_ranges.is_empty() {
+            return Self {
+                starts: vec![],
+                ends: vec![],
+                lengths: vec![],
+                qual: vec![],
+                reference_starts: vec![],
+                reference_ends: vec![],
+                reference_lengths: vec![],
+                seq_len: 0,
+                reverse: false,
+            };
+        }
+        // check properties that must be the same
+        let reverse = multiple_ranges[0].reverse;
+        let seq_len = multiple_ranges[0].seq_len;
+        for r in multiple_ranges.iter() {
+            assert_eq!(r.reverse, reverse);
+            assert_eq!(r.seq_len, seq_len);
+        }
+        // get the other properties
+        let starts = multiple_ranges.iter().flat_map(|r| r.starts.clone());
+        let ends = multiple_ranges.iter().flat_map(|r| r.ends.clone());
+        let lengths = multiple_ranges.iter().flat_map(|r| r.lengths.clone());
+        let qual = multiple_ranges.iter().flat_map(|r| r.qual.clone());
+        let reference_starts = multiple_ranges
+            .iter()
+            .flat_map(|r| r.reference_starts.clone());
+        let reference_ends = multiple_ranges
+            .iter()
+            .flat_map(|r| r.reference_ends.clone());
+        let reference_lengths = multiple_ranges
+            .iter()
+            .flat_map(|r| r.reference_lengths.clone());
+
+        #[allow(clippy::type_complexity)]
+        let mut combo: Vec<(
+            Option<i64>,
+            Option<i64>,
+            Option<i64>,
+            u8,
+            Option<i64>,
+            Option<i64>,
+            Option<i64>,
+        )> = izip!(
+            starts,
+            ends,
+            lengths,
+            qual,
+            reference_starts,
+            reference_ends,
+            reference_lengths
+        )
+        .collect();
+        // sort by start position
+        combo.sort_by_key(|(s, _e, _l, _q, _r_s, _r_e, _r_l)| *s);
+        // unzip
+        let (starts, ends, lengths, qual, reference_starts, reference_ends, reference_lengths) =
+            multiunzip(combo);
+
+        Self {
+            starts,
+            ends,
+            lengths,
+            qual,
+            reference_starts,
+            reference_ends,
+            reference_lengths,
+            seq_len,
+            reverse,
+        }
+    }
+}
+
+impl<'a> IntoIterator for &'a Ranges {
+    type Item = (i64, i64, i64, Option<(i64, i64, i64)>);
+    type IntoIter = RangesIterator<'a>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        RangesIterator {
+            row: self,
+            index: 0,
+        }
+    }
+}
+
+pub struct RangesIterator<'a> {
+    row: &'a Ranges,
+    index: usize,
+}
+
+impl<'a> Iterator for RangesIterator<'a> {
+    type Item = (i64, i64, i64, Option<(i64, i64, i64)>);
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index >= self.row.starts.len() {
+            return None;
+        }
+        let start = self.row.starts[self.index]?;
+        let end = self.row.ends[self.index]?;
+        let length = self.row.lengths[self.index]?;
+        let reference_start = self.row.reference_starts[self.index];
+        let reference_end = self.row.reference_ends[self.index];
+        let reference_length = self.row.reference_lengths[self.index];
+        let reference = match (reference_start, reference_end, reference_length) {
+            (Some(s), Some(e), Some(l)) => Some((s, e, l)),
+            _ => None,
+        };
+        self.index += 1;
+        Some((start, end, length, reference))
+    }
+}
--- src/cli.rs
+++ src/cli.rs
@@ -73,71 +73,7 @@ pub struct Cli {
 pub enum Commands {
     /// Predict m6A positions using HiFi kinetics data and encode the results in the MM and ML bam tags. Also adds nucleosome (nl, ns) and MTase sensitive patches (al, as).
     #[clap(visible_aliases = &["m6A", "m6a"])]
-    PredictM6A {
-        /// Bam HiFi file with kinetics
-        #[clap(default_value = "-")]
-        bam: String,
-        /// Output bam file with m6A calls in new/extended MM and ML bam tags
-        #[clap(default_value = "-")]
-        out: String,
-        /// Minium nucleosome length
-        #[clap(short, long, default_value = NUC_LEN)]
-        nucleosome_length: i64,
-        /// Minium nucleosome length when combining over a single m6A
-        #[clap(short, long, default_value = COMBO_NUC_LEN)]
-        combined_nucleosome_length: i64,
-        /// Minium distance needed to add to an already existing nuc by crossing an m6a
-        #[clap(long, default_value = MIN_DIST_ADDED)]
-        min_distance_added: i64,
-        /// Minimum distance from the end of a fiber to call a nucleosome or MSP
-        #[clap(short, long, default_value = DIST_FROM_END)]
-        distance_from_end: i64,
-        /// Most m6A events we can skip over to get to the nucleosome length when using D-segment algorithm. 2 is often a good value, negative values disable D-segment for the simple caller.
-        #[clap(long, default_value = ALLOWED_SKIPS, hide = true)]
-        allowed_m6a_skips: i64,
-        /// Keep hifi kinetics data
-        #[clap(short, long)]
-        keep: bool,
-        /// Set a minimum ML score to keep on instead of using the model specific minimum ML score.
-        #[clap(short, long, help_heading = "Developer-Options")]
-        min_ml_score: Option<u8>,
-        /// Keep all m6A calls regardless of how low the ML value is
-        #[clap(short, long, help_heading = "Developer-Options")]
-        all_calls: bool,
-        /// Use the XGBoost model for prediction
-        #[clap(long, help_heading = "Developer-Options")]
-        xgb: bool,
-        /// Use the CNN model for prediction
-        #[clap(long, help_heading = "Developer-Options")]
-        cnn: bool,
-        /// Use the semi-supervised CNN model for prediction [default: true]
-        #[clap(
-            short,
-            long,
-            default_value_ifs([
-                ("cnn", "true", "false"),
-                ("xgb", "false", "true"),
-            ]),
-            help_heading = "Developer-Options",
-        )]
-        semi: bool,
-        /// Add a bam tag (mp) with the full floating point predictions of the ML model
-        ///
-        /// For debugging only.
-        #[clap(short, long, help_heading = "Developer-Options")]
-        full_float: bool,
-        /// Number of reads to include in batch prediction
-        ///
-        /// Increasing improves GPU performance at the cost of memory.
-        #[clap(
-            short,
-            long,
-            default_value = "1",
-            default_value_if("cnn", "true", "1"),
-            help_heading = "Developer-Options"
-        )]
-        batch_size: usize,
-    },
+    PredictM6A(PredictM6AOptions),
     /// Add nucleosomes to a bam file with m6a predictions
     AddNucleosomes(AddNucleosomeOptions),
     /// Add FIREs (Fiber-seq Inferred Regulatory Elements) to a bam file with m6a predictions
@@ -147,75 +83,13 @@ pub enum Commands {
     /// Extract fiberseq data into plain text files.
     /// See https://fiberseq.github.io/fibertools-rs/docs/extract.html for a description of the outputs.
     #[clap(visible_aliases = &["ex", "e"])]
-    Extract {
-        /// Input fiberseq bam file. If no path is provided extract will read bam data from stdin.
-        #[arg(default_value = "-", value_hint = ValueHint::AnyPath)]
-        bam: String,
-        /// Report in reference sequence coordinates
-        #[clap(short, long, default_value = "true",
-            default_value_ifs([
-                ("molecular", "true", "false"),
-                ("molecular", "false", "true"),
-            ]))
-        ]
-        reference: bool,
-        /// Report positions in the molecular sequence coordinates
-        #[clap(long, default_value = "false")]
-        molecular: bool,
-        /// Minium score in the ML tag to include in the output
-        #[clap(short, long, default_value = MIN_ML_SCORE)]
-        min_ml_score: u8,
-        /// Output path for m6a bed12
-        #[clap(long)]
-        m6a: Option<String>,
-        /// Output path for 5mC (CpG, primrose) bed12
-        #[clap(short, long)]
-        cpg: Option<String>,
-        /// Output path for methylation sensitive patch (msp) bed12
-        #[clap(long)]
-        msp: Option<String>,
-        /// Output path for nucleosome bed12
-        #[clap(short, long)]
-        nuc: Option<String>,
-        /// Output path for a tabular format including "all" fiberseq information in the bam
-        #[clap(short, long)]
-        all: Option<String>,
-        /// Include per base quality scores in "fiber_qual"
-        #[clap(short, long, help_heading = "All-Format-Options")]
-        quality: bool,
-        /// Add the full floating point predictions of the ML model
-        #[clap(short, long, help_heading = "All-Format-Options")]
-        full_float: bool,
-        /// Simplify output by remove fiber sequence
-        #[clap(short, long, help_heading = "All-Format-Options")]
-        simplify: bool,
-    },
-
-    #[clap(visible_aliases = &["c", "ct"])]
+    Extract(ExtractOptions),
     /// This command centers fiberseq data around given reference positions. This is useful for making aggregate m6A and CpG observations, as well as visualization of SVs.
     ///  See https://fiberseq.github.io/fibertools-rs/docs/center.html for a description of the output.
-    Center {
-        /// Aligned Fiber-seq bam file.
-        bam: String,
-        /// Bed file on which to center fiberseq reads. Data is adjusted to the start position of the bed file and corrected for strand if the strand is indicated in the 6th column of the bed file. The 4th column will also be checked for the strand but only after the 6th is.        
-        /// If you include strand information in the 4th (or 6th) column it will orient data accordingly and use the end position of bed record instead of the start if on the minus strand. This means that profiles of motifs in both the forward and minus orientation will align to the same central position.
-        bed: String,
-        /// Minium score in the ML tag to include in the output
-        #[clap(short, long, default_value = MIN_ML_SCORE)]
-        min_ml_score: u8,
-        /// Set a maximum distance from the start of the motif to keep a feature
-        #[clap(short, long)]
-        dist: Option<i64>,
-        /// Provide data in wide format, one row per read
-        #[clap(short, long)]
-        wide: bool,
-        /// Return relative reference position instead of relative molecular position
-        #[clap(short, long)]
-        reference: bool,
-        /// Replace the sequence output column with just "N".
-        #[clap(short, long)]
-        simplify: bool,
-    },
+    #[clap(visible_aliases = &["c", "ct"])]
+    Center(CenterOptions),
+    /// Make decorated bed files for fiberseq data
+    TrackDecorators(DecoratorOptions),
     /// Remove HiFi kinetics tags from the input bam file
     ClearKinetics {
         /// Bam HiFi file with kinetics
@@ -237,8 +111,6 @@ pub enum Commands {
         /// base modification to strip out of the bam file
         basemod: String,
     },
-    /// Make decorated bed files for fiberseq data
-    TrackDecorators(DecoratorOptions),
     /// Make command line completions
     #[clap(hide = true)]
     Completions {
@@ -263,6 +135,73 @@ pub fn make_cli_parse() -> Cli {
 
 pub fn make_cli_app() -> Command {
     Cli::command()
+}
+
+#[derive(Args, Debug, PartialEq, Eq)]
+pub struct PredictM6AOptions {
+    /// Bam HiFi file with kinetics
+    #[clap(default_value = "-")]
+    pub bam: String,
+    /// Output bam file with m6A calls in new/extended MM and ML bam tags
+    #[clap(default_value = "-")]
+    pub out: String,
+    /// Minium nucleosome length
+    #[clap(short, long, default_value = NUC_LEN)]
+    pub nucleosome_length: i64,
+    /// Minium nucleosome length when combining over a single m6A
+    #[clap(short, long, default_value = COMBO_NUC_LEN)]
+    pub combined_nucleosome_length: i64,
+    /// Minium distance needed to add to an already existing nuc by crossing an m6a
+    #[clap(long, default_value = MIN_DIST_ADDED)]
+    pub min_distance_added: i64,
+    /// Minimum distance from the end of a fiber to call a nucleosome or MSP
+    #[clap(short, long, default_value = DIST_FROM_END)]
+    pub distance_from_end: i64,
+    /// Most m6A events we can skip over to get to the nucleosome length when using D-segment algorithm. 2 is often a good value, negative values disable D-segment for the simple caller.
+    #[clap(long, default_value = ALLOWED_SKIPS, hide = true)]
+    pub allowed_m6a_skips: i64,
+    /// Keep hifi kinetics data
+    #[clap(short, long)]
+    pub keep: bool,
+    /// Set a minimum ML score to keep on instead of using the model specific minimum ML score.
+    #[clap(short, long, help_heading = "Developer-Options")]
+    pub min_ml_score: Option<u8>,
+    /// Keep all m6A calls regardless of how low the ML value is
+    #[clap(short, long, help_heading = "Developer-Options")]
+    pub all_calls: bool,
+    /// Use the XGBoost model for prediction
+    #[clap(long, help_heading = "Developer-Options")]
+    pub xgb: bool,
+    /// Use the CNN model for prediction
+    #[clap(long, help_heading = "Developer-Options")]
+    pub cnn: bool,
+    /// Use the semi-supervised CNN model for prediction [default: true]
+    #[clap(
+          short,
+          long,
+          default_value_ifs([
+              ("cnn", "true", "false"),
+              ("xgb", "false", "true"),
+          ]),
+          help_heading = "Developer-Options",
+      )]
+    pub semi: bool,
+    /// Add a bam tag (mp) with the full floating point predictions of the ML model
+    ///
+    /// For debugging only.
+    #[clap(short, long, help_heading = "Developer-Options")]
+    pub full_float: bool,
+    /// Number of reads to include in batch prediction
+    ///
+    /// Increasing improves GPU performance at the cost of memory.
+    #[clap(
+        short,
+        long,
+        default_value = "1",
+        default_value_if("cnn", "true", "1"),
+        help_heading = "Developer-Options"
+    )]
+    pub batch_size: usize,
 }
 
 #[derive(Args, Debug, PartialEq, Eq)]
@@ -374,4 +313,73 @@ pub struct FootprintOptions {
     pub yaml: String,
     /// Output bam
     pub out: String,
+}
+
+#[derive(Args, Debug, PartialEq, Eq)]
+pub struct CenterOptions {
+    /// Aligned Fiber-seq bam file.
+    pub bam: String,
+    /// Bed file on which to center fiberseq reads. Data is adjusted to the start position of the bed file and corrected for strand if the strand is indicated in the 6th column of the bed file. The 4th column will also be checked for the strand but only after the 6th is.        
+    /// If you include strand information in the 4th (or 6th) column it will orient data accordingly and use the end position of bed record instead of the start if on the minus strand. This means that profiles of motifs in both the forward and minus orientation will align to the same central position.
+    pub bed: String,
+    /// Minium score in the ML tag to include in the output
+    #[clap(short, long, default_value = MIN_ML_SCORE)]
+    pub min_ml_score: u8,
+    /// Set a maximum distance from the start of the motif to keep a feature
+    #[clap(short, long)]
+    pub dist: Option<i64>,
+    /// Provide data in wide format, one row per read
+    #[clap(short, long)]
+    pub wide: bool,
+    /// Return relative reference position instead of relative molecular position
+    #[clap(short, long)]
+    pub reference: bool,
+    /// Replace the sequence output column with just "N".
+    #[clap(short, long)]
+    pub simplify: bool,
+}
+
+#[derive(Args, Debug, PartialEq, Eq)]
+pub struct ExtractOptions {
+    /// Input fiberseq bam file. If no path is provided extract will read bam data from stdin.
+    #[arg(default_value = "-", value_hint = ValueHint::AnyPath)]
+    pub bam: String,
+    /// Report in reference sequence coordinates
+    #[clap(short, long, default_value = "true",
+          default_value_ifs([
+              ("molecular", "true", "false"),
+              ("molecular", "false", "true"),
+          ]))
+      ]
+    pub reference: bool,
+    /// Report positions in the molecular sequence coordinates
+    #[clap(long, default_value = "false")]
+    pub molecular: bool,
+    /// Minium score in the ML tag to include in the output
+    #[clap(short, long, default_value = MIN_ML_SCORE)]
+    pub min_ml_score: u8,
+    /// Output path for m6a bed12
+    #[clap(long)]
+    pub m6a: Option<String>,
+    /// Output path for 5mC (CpG, primrose) bed12
+    #[clap(short, long)]
+    pub cpg: Option<String>,
+    /// Output path for methylation sensitive patch (msp) bed12
+    #[clap(long)]
+    pub msp: Option<String>,
+    /// Output path for nucleosome bed12
+    #[clap(short, long)]
+    pub nuc: Option<String>,
+    /// Output path for a tabular format including "all" fiberseq information in the bam
+    #[clap(short, long)]
+    pub all: Option<String>,
+    /// Include per base quality scores in "fiber_qual"
+    #[clap(short, long, help_heading = "All-Format-Options")]
+    pub quality: bool,
+    /// Add the full floating point predictions of the ML model
+    #[clap(short, long, help_heading = "All-Format-Options")]
+    pub full_float: bool,
+    /// Simplify output by remove fiber sequence
+    #[clap(short, long, help_heading = "All-Format-Options")]
+    pub simplify: bool,
 }
