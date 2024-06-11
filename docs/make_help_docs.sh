cf03b1d2e1b3915d37c3e8411f81845355043e0c
Mitchell Robert Vollger <mrvollger@gmail.com>
2024-06-10 22:26:07 -0700

    feat: starting the book

--- docs/make_help_docs.sh
+++ docs/make_help_docs.sh
@@ -6,6 +6,7 @@ export DYLD_LIBRARY_PATH=${LIBTORCH}/lib:$LD_LIBRARY_PATH
 
 out="src/help.md"
 echo "# Help pages for fibertools subcommands" >$out
+echo "<!-- toc -->" >>$out
 echo "" >>$out
 
 for subcommand in "" "predict-m6a" "fire" "extract" "center" "add-nucleosomes" "footprint" "clear-kinetics" "strip-basemods" "track-decorators" "pileup"; do
