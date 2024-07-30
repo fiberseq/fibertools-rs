3eb0bf4ed4fc45e843f747b8277168777b982e02
Mitchell Robert Vollger <mrvollger@gmail.com>
2024-07-30 14:19:53 -0700

    chore: clippy

--- src/utils/input_bam.rs
+++ src/utils/input_bam.rs
@@ -25,7 +25,7 @@ pub struct FiberFilters {
     #[clap(long="ml", alias="min-ml-score", default_value = MIN_ML_SCORE, help_heading = "BAM-Options", env="FT_MIN_ML_SCORE")]
     pub min_ml_score: u8,
     // @SHANE this is where we would add more filtering options
-    #[clap(short, long)]
+    #[clap(short, long, help_heading = "BAM-Options", hide = true)]
     pub filter_expression: Option<String>,
 }
 
