6a37478a84193cafe3dc2209b862d094af59cf8a
Mitchell Robert Vollger <mrvollger@gmail.com>
2024-08-20 10:32:50 -0700

    chore: help msg.

--- src/cli/qc_opts.rs
+++ src/cli/qc_opts.rs
@@ -6,8 +6,12 @@ use std::fmt::Debug;
 pub struct QcOpts {
     #[clap(flatten)]
     pub input: InputBam,
+    /// Output text file with QC metrics. The format is a tab-separated file with the following columns: "statistic\tvalue\tcount" where "statistic" is the name of the metric, "value" is the value of the metric, and "count" is the number of times the metric was observed.
     #[clap(default_value = "-")]
     pub out: String,
+    /// In the output include a measure of the number of m6A events per MSPs of a given size.
+    /// The output format is: "m6a_per_msp_size\t{m6A count},{MSP size},{is a FIRE}\t{count}"
+    /// e.g. "m6a_per_msp_size\t35,100,false\t100"
     #[clap(short, long)]
     pub m6a_per_msp: bool,
 }
