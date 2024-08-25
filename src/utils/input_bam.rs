d79ebbe00f401d8921d92197bc0fc6392a098208
Shane Neph <sjneph@gmail.com>
2024-08-24 18:37:42 -0700

    fix typo

--- src/utils/input_bam.rs
+++ src/utils/input_bam.rs
@@ -25,7 +25,7 @@ pub struct FiberFilters {
     /// Example: filter to nucleosomes with lengths greater than 150 bp
     ///   -x "len(nuc)>150"
     /// Example: filter to msps with lengths between 30 and 49 bp
-    ///   -x "len(nuc)=30:50"
+    ///   -x "len(msp)=30:50"
     /// Example: combine 2+ filter expressions
     ///   -x "len(nuc)<150,len(msp)=30:50"
     /// Filtering expressions support len() and qual() functions over msp, nuc, m6a, cpg
