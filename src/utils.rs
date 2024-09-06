4802219510c7a66ec700ede78ae115cb18a722ae
Mitchell Robert Vollger <mrvollger@gmail.com>
2024-09-06 07:32:28 -0700

    expression filter (#57)

--- src/utils.rs
+++ src/utils.rs
@@ -2,7 +2,9 @@ pub mod bamlift;
 pub mod bamranges;
 pub mod basemods;
 pub mod bio_io;
-pub mod filter_expressions;
 pub mod fire;
 pub mod input_bam;
 pub mod nucleosome;
+
+// test modules for expressions
+pub mod ftexpression;
