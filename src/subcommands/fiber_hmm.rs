2ab92b8d23c3d03c2b7a51241091a0f19391e365
Mitchell R. Vollger <mvollger@gmai.com>
2025-07-07 07:28:30 -0700

    remove clippy warnings

--- src/subcommands/fiber_hmm.rs
+++ src/subcommands/fiber_hmm.rs
@@ -8,12 +8,13 @@ pub fn run_fiber_hmm(opts: &mut FiberHmmOptions) -> Result<(), Error> {
     let mut out = opts.input.bam_writer(&opts.out);
     for fiber in opts.input.fibers(&mut bam) {
         // m6a positions on the forward strand
-        let m6a = fiber.m6a.get_forward_starts();
+        let _m6a = fiber.m6a.get_forward_starts();
         // forward strand sequence
         let mut seq = fiber.record.seq().as_bytes();
         if fiber.record.is_reverse() {
             seq = revcomp(seq);
         }
+        log::trace!("Run HMM on fiber: {:?}", seq);
 
         // TODO hmm stuff on the one read
 
